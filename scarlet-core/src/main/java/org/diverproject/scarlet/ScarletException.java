17a4235f2b8f78f4815cb46e08f6dfd3c9427417
Andrew Mello <driwundernight@gmail.com>
2020-11-08 09:13:41 -0300

    Updated ScarletException language attribute
    - Sonar recommend be transient because isn't Serializable and final because will not be changed
    - Removed unused constructors

--- scarlet-core/src/main/java/org/diverproject/scarlet/ScarletException.java
+++ scarlet-core/src/main/java/org/diverproject/scarlet/ScarletException.java
@@ -1,50 +1,26 @@
 package org.diverproject.scarlet;
 
+import lombok.Getter;
 import org.diverproject.scarlet.language.Language;
 
 public class ScarletException extends Exception {
 
 	private static final long serialVersionUID = -6658438626365742664L;
 
-	private transient Language language;
-
-	public ScarletException(Language language) {
-		super(language.getFormat());
-
-		this.setLanguage(language);
-	}
+	@Getter
+	private final transient Language language;
 
 	public ScarletException(Language language, Object... args) {
 		super(String.format(language.getFormat(), args));
 
-		this.setLanguage(language);
-	}
-
-	public ScarletException(Throwable e) {
-		super(e.getMessage());
-
-		this.setStackTrace(e.getStackTrace());
-	}
-
-	public ScarletException(Throwable e, Language language) {
-		super(language.getFormat());
-
-		this.setLanguage(language);
-		this.setStackTrace(e.getStackTrace());
+		this.language = language;
 	}
 
 	public ScarletException(Throwable e, Language language, Object... args) {
 		super(String.format(language.getFormat(), args) + "; " + e.getClass().getSimpleName() + " - " + e.getMessage());
 
-		this.setLanguage(language);
+		this.language = language;
 		this.setStackTrace(e.getStackTrace());
 	}
 
-	private void setLanguage(Language language) {
-		this.language = language;
-	}
-
-	public int getCode() {
-		return language.getCode();
-	}
 }
